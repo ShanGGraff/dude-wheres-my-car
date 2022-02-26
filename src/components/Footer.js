220260125aed3753d812e40c08e3e4bd521ff462
Shane Graff <copellius@gmail.com>
2022-02-26 15:38:42 -0800

    Fixed Footer.js so it is now sticky at the bottom of the page. Need to reasource styling more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+
 import {
   Box,
   Container,
@@ -7,42 +8,51 @@ import {
   FooterLink,
   Heading,
 } from "./FooterStyles";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>Dude footer</p>
   
-const Footer = () => {
-  return (
-    <Box>
-      <h1 style={{ color: "white", 
-        textAlign: "center", 
-        marginTop: "-50px" }}>
-        A resource for finding your missing car in Portland Oregon.
-      </h1>
-      <Container>
-        <Row>
-          <Column>
-            <Heading>Other Resources</Heading>
-            <FooterLink href="#">TBD</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Services</Heading>
-            <FooterLink href="#">TBD</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Contact Us</Heading>
-            <FooterLink href="#">Portland</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Social Media</Heading>
-            <FooterLink href="#">
-              <i className="ShaneDude">
-                <span style={{ marginLeft: "10px" }}>
-                  LinkedIn
-                </span>
-              </i>
-            </FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </Box>
-  );
-};
+  </footer>
+);
+  
 export default Footer;
+  
+// const Footer = () => {
+//   return (
+//     <Box>
+//       <h1 style={{ color: "white", 
+//         textAlign: "center", 
+//         marginTop: "-50px" }}>
+//         A resource for finding your missing car in Portland Oregon.
+//       </h1>
+//       <Container>
+//         <Row>
+//           <Column>
+//             <Heading>Other Resources</Heading>
+//             <FooterLink href="#">TBD</FooterLink>
+//           </Column>
+//           <Column>
+//             <Heading>Services</Heading>
+//             <FooterLink href="#">TBD</FooterLink>
+//           </Column>
+//           <Column>
+//             <Heading>Contact Us</Heading>
+//             <FooterLink href="#">Portland</FooterLink>
+//           </Column>
+//           <Column>
+//             <Heading>Social Media</Heading>
+//             <FooterLink href="#">
+//               <i className="ShaneDude">
+//                 <span style={{ marginLeft: "10px" }}>
+//                   LinkedIn
+//                 </span>
+//               </i>
+//             </FooterLink>
+//           </Column>
+//         </Row>
+//       </Container>
+//     </Box>
+//   );
+// };
+// export default Footer;
