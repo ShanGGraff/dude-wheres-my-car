25015ed08eac1d723f10c35df03b5a3118362250
Shane Graff <copellius@gmail.com>
2022-02-26 12:51:17 -0800

    Change <main> into it's own component. Change App.js to import it. Footer still not at bottom of page

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import car from '../img/StolenCar.jpg';
+// import car from '../img/StolenCar.jpg';
 import ReactDOM from "react-dom";
 // import App from "./App";
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 
 function App(){
   return (
     <React.Fragment>
       <Header/>
-      {/* <div>
-        <img src={car} alt="picture of stolen car and police officer in Portland Oregon" />
-      </div>
-      <hr/>  */}
-      <main>
-        <div>
-          <img src={car} alt="picture of stolen car and police officer in Portland Oregon" />
-        </div>
-      </main>
-      <Footer />
+      <Main/>
+      <Footer/>
     </React.Fragment>
     
   );
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import car from '../img/StolenCar.jpg';
+
+
+
+export default function Main() {
+  return (
+  <div>
+    <img src={car} alt="picture of stolen car and police officer in Portland Oregon" />
+  </div>
+  );
+}
