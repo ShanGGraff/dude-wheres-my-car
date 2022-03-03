c79f9c352fcbc9b160d74c415af353af16418cdc
Shane Graff <copellius@gmail.com>
2022-03-03 11:03:44 -0800

    BUGFIX: Fixed typos in CarControl.js and EditCarForm

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import {combineReducers} from "redux";
 // import {firebaseReducer} from "react-redux-firebase";
 // import {firestoreReducer} from "redux-firestore";
 import formVisibleReducer from './form-visible-reducer';
-import mainCarList from './car-list-reducer';
+import carListReducer from './car-list-reducer';
 
 const rootReducer = combineReducers({
-  fromVisibleOnPage: formVisibleReducer,
-  mainCarList: CarListReducer
+  formVisibleOnPage: formVisibleReducer,
+  mainCarList: carListReducer
   // firebase: firebaseReducer,
   // firestore: firestoreReducer
 });
