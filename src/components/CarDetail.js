b90f748e2d2fc878dabe6f9b3b7d0b7d8cc44f17
Shane Graff <copellius@gmail.com>
2022-03-03 17:07:37 -0800

    Added code for CarDetail.js

---
+++ src/components/CarDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CarDetail(props){
+  const { car, onClickingDelete } = props;
+  
+  return (
+    <React.Fragment>
+      <h1>Car Detail</h1>
+      <h3>{car.make} - {car.model}</h3>
+      <p><em>{car.vin}</em></p>
+      <button onClick={ props.onClickingEdit }>Update Car</button>
+      <button onClick={()=> onClickingDelete(car.id) }>Delete car</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+CarDetail.propTypes = {
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default CarDetail;
