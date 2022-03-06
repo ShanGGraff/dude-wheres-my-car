7712db76f35222845d4ebbd6f595c2520af88ae0
Shane Graff <copellius@gmail.com>
2022-03-06 15:01:11 -0800

    Changed App.js to use Footer.js and to put Sign-up page as the root

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 // import ReactDOM from "react-dom";
 import Header from "./Header";
 import Signup from "./auth/Signup";
-// import Footer from "./Footer";
-// import Main from "./Main";
+import Footer from "./Footer";
+import Main from "./Main";
 import Signin from "./auth/Signin";
 import CarControl from "./CarControl";
 
 function App(){
   return (
-    <Router>
-    <Header />
-      <Route path="/signup" element={<Signup/>} />
-  </Router>
+    // <Router>
+    // <Header />
+    //   <Route path="/signup" element={<Signup/>} />
+    // </Router>
 
     // <React.Fragment>
     //   <Header />
-    //   <CarControl />
+    //   {/* <Signup /> */}
+    //   <Footer />
     // </React.Fragment>
 
-
+  <Router>
+    <Header />
+    
+      <Routes>
+        {/* <Route path="/signin" element={<Signin />} /> */}
+        <Route path="/" element={<Signup />} />
+        // <Route path="/" element={<CarControl />} />
+      </Routes>
+    <Footer />
+  </Router>
   );
 }
 
