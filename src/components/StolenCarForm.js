19062b18ccdc2b9c4fc1a96b27d1682fa43af21e
Shane Graff <copellius@gmail.com>
2022-03-06 07:21:05 -0800

    Change reducers/index.js and StolenCarForm.js to work with firestore

--- src/components/StolenCarForm.js
+++ src/components/StolenCarForm.js
@@ -1,33 +1,48 @@
 import React from "react";
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-import Moment from 'moment';
+// import Moment from 'moment';
+import { useFirestore } from 'react-redux-firebase';
 
 function StolenCarForm(props){
+  const firestore = useFirestore();
 
   function handleStolenCarFormSubmission(event) {
     event.preventDefault();
-    props.onNewCarCreation({
-      make: event.target.make.value,
-      model: event.target.make.model,
-      year: event.target.make.year,
-      vin: event.target.make.vin,
-      license: event.target.make.license,
-      // carimage: event.target.make.carimage,
-      date: event.target.make.date,
-      location: event.target.location.value,
-      id: v4(),
-      timeOpen: new Moment(),
-      formattedWaitTime: new Moment().fromNow(true)
-    });
+    props.onNewCarCreation();
+    return firestore.collection('cars').add(
+      {
+        make: event.target.make.value, 
+        model: event.target.model, 
+        year: event.target.year.value, 
+        vin: event.target.vin.value, 
+        license: event.target.license,
+        // carimage: event.target.carimage,
+        date: event.target.date,
+        location: event.target.location
+      }
+    );
+    //{
+    //   make: event.target.make.value,
+    //   model: event.target.make.model,
+    //   year: event.target.make.year,
+    //   vin: event.target.make.vin,
+    //   license: event.target.make.license,
+    //   carimage: event.target.make.carimage,
+    //   date: event.target.make.date,
+    //   location: event.target.location.value,
+    //   id: v4(),
+    //   timeOpen: new Moment(),
+    //   formattedWaitTime: new Moment().fromNow(true)
+    // });
   }
 
   return (
     <React.Fragment>
       <ReusableForm 
         formSubmissionHandler={handleStolenCarFormSubmission}
-        buttonText="Help!" />
+        buttonText="Submit!" />
     </React.Fragment>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
 import {combineReducers} from "redux";
 // import {firebaseReducer} from "react-redux-firebase";
-// import {firestoreReducer} from "redux-firestore";
+import {firestoreReducer} from "redux-firestore";
 import formVisibleReducer from './form-visible-reducer';
 import carListReducer from './car-list-reducer';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainCarList: carListReducer
+  mainCarList: carListReducer,
   // firebase: firebaseReducer,
-  // firestore: firestoreReducer
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
